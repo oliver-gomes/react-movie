1b455b426f0ffdf05d3a0f433285350fe5ccef9f
Oliver Gomes <ogomesphoto@gmail.com>
2018-12-30 00:38:22 -0500

    Home setup

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default Home;

f98f451695b4507d9c8d999fd0f836b6fde4e0cf
Oliver Gomes <ogomesphoto@gmail.com>
2018-12-30 16:19:30 -0500

    condition statement for FourColGrid and LoadMoreBtn update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -95,10 +95,33 @@ class Home extends Component {
             <SearchBar callback={this.searchItems} />
           </div>
         ) : null}
-
-        <FourColGrid />
-        <Spinner />
-        <LoadMoreBtn />
+        <div className="rmdb-home-grid">
+          <FourColGrid
+            header={this.state.searchTerm ? "Search Result" : "Popular Movies"}
+            loading={this.state.loading}
+          >
+            {this.state.movies.map((element, i) => {
+              return (
+                <MovieThumb
+                  key={i}
+                  clickable={true}
+                  image={
+                    element.poster_path
+                      ? `${IMAGE_BASE_URL}${POSTER_SIZE}/${element.poster_path}`
+                      : "./images/no_image.jpg"
+                  }
+                  movieId={element.id}
+                  movieName={element.original_title}
+                />
+              );
+            })}
+          </FourColGrid>
+          {this.state.loading ? <Spinner /> : null}
+          {this.state.currentPage <= this.state.totalPages &&
+          !this.state.loading ? (
+            <LoadMoreBtn text="Load More" onClick={this.loadMoreItems} />
+          ) : null}
+        </div>
       </div>
     );
   }

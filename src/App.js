9b93fd7e5055be60986abf707009a4272885375e
Oliver Gomes <ogomesphoto@gmail.com>
2018-12-30 00:30:28 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reactdfdsfasdf
+            Learn React
           </a>
         </header>
       </div>

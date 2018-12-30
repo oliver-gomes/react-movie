da7718029303e75a355c89f786fdb8e6c7a5394f
Oliver Gomes <ogomesphoto@gmail.com>
2018-12-30 01:36:19 -0500

    componentDidMount setup and fetch API setup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,7 +29,39 @@ class Home extends Component {
   componentDidMount() {
     this.setState({ loading: true });
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
   }
+
+  loadMoreItems = () => {
+    let endpoint = "";
+    this.setState({
+      loading: true
+    });
+
+    if (this.state.searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this
+        .state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${
+        this.state.searchTerm
+      }$page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
+  };
+
+  fetchItems = endpoint => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          movies: [...this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        });
+      });
+  };
 
   render() {
     return (

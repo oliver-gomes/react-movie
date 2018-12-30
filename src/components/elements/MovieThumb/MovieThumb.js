474f5e4e5994b128f140bed03d678f21710c9250
Oliver Gomes <ogomesphoto@gmail.com>
2018-12-30 16:18:22 -0500

    MovieThumb props setup

--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./MovieThumb.css";
 
 const MovieThumb = props => {
-  return <div>MovieThumb</div>;
+  return (
+    <div className="rmdb-moviethumb">
+      <img src={props.image} alt="moviethumb" />
+    </div>
+  );
 };
 
 export default MovieThumb;
